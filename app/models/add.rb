e30b2eaf4990ba98d16cdb15a9404e39e0ea4f8b
Priyans Desai <priyansdesai@berkeley.edu>
2018-11-27 00:00:39 -0800

    New Changes

---
+++ app/models/add.rb
@@ -0,0 +1,4 @@
+class Add < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
     has_one_attached :image
     belongs_to :category, optional: true
     belongs_to :user, optional: true
+    has_many :adds
+    has_many :users_added, through: :adds, source: :user
 end
